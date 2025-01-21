7e5b0f8807d7fcb3b5049f3adb4c10611247af60
DiveshR <divesh20july@gmail.com>
2025-01-21 17:10:42 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,39 @@
+print("Hello World!")
+print("Hello World!\nHello World!\nHello World!")
+
+# Concatenate
+#  print("Hello"+ " "+  "Python") # IndentationError: unexpected indent
+print("ßßNotes from Day 1")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \n and the letter n")
+
+# Input data
+# input("What is your name?")
+print("Hello " + input("What is your name?") + "!")
+
+# Python variables
+name = input("what is your name?")
+print(name)
+
+name = "john"
+print(name)
+
+name = "Dee"
+print(name)
+
+# string length - get the length of the string
+print(len(input("Enter your name?")))
+username = input("Enter your name?")
+length = len(username)
+print(length)
+
+name = "Steve"
+name2 = "Glen"
+name3=name
+name=name2
+name2=name3
+print(name + " " +name2)
